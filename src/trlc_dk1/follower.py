48e6d8de57c377a8d9deb3f4ee33368c66e31288
J4nn1K <jannik@grothusen.de>
2025-09-06 12:29:07 +0200

    Refactor default control values and gripper control

--- src/trlc_dk1/follower.py
+++ src/trlc_dk1/follower.py
@@ -15,6 +15,7 @@ from trlc_dk1.motors.DM_Control_Python.DM_CAN import *
 
 logger = logging.getLogger(__name__)
 
+
 def map_range(x: float, in_min: float, in_max: float, out_min: float, out_max: float) -> float:
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
@@ -23,29 +24,31 @@ def map_range(x: float, in_min: float, in_max: float, out_min: float, out_max: f
 @dataclass
 class DK1FollowerConfig(RobotConfig):
     port: str
-    
     disable_torque_on_disconnect: bool = False
-
-    # `max_relative_target` limits the magnitude of the relative positional target vector for safety purposes.
-    # Set this to a positive scalar to have the same value for all motors, or a list that is the same length as
-    # the number of motors in your follower arms.
-    max_relative_target: int | None = None
-
-    # cameras
+    joint_velocity_scaling: float = 0.2
+    max_gripper_torque: float = 1.0 # Nm (/0.00875m spur gear radius = 114N gripper force)
     cameras: dict[str, CameraConfig] = field(default_factory=dict)
-    
+
 
 class DK1Follower(Robot):
     """
     TRLC-DK1 Follower Arm designed by The Robot Learning Company.
     """
-    
+
     config_class = DK1FollowerConfig
     name = "dk1_follower"
-    
+
     def __init__(self, config: DK1FollowerConfig):
         super().__init__(config)
         
+        # Constants for EMIT control
+        self.DM4310_TORQUE_CONSTANT = 0.945  # Nm/A
+        self.EMIT_VELOCITY_SCALE = 100  # rad/s
+        self.EMIT_CURRENT_SCALE = 1000  # A
+        
+        self.DM4310_SPEED = 200/60*2*np.pi  # rad/s (200 rpm)
+        self.DM4340_SPEED = 52.5/60*2*np.pi  # rad/s (52.5 rpm)
+
         self.config = config
         self.motors = {
             "joint_1": Motor(DM_Motor_Type.DM4340, 0x01, 0x11),
@@ -59,23 +62,22 @@ class DK1Follower(Robot):
         self.control = None
         self.serial_device = None
         self.bus_connected = False
-        
+
         self.gripper_open_pos = 0.0
-        self.gripper_closed_pos = -4.6
-        
+        self.gripper_closed_pos = -4.7
+
         self.cameras = make_cameras_from_configs(config.cameras)
-        
-    
+
     @property
     def _motors_ft(self) -> dict[str, type]:
         return {f"{motor}.pos": float for motor in self.motors}
-    
+
     @property
     def _cameras_ft(self) -> dict[str, tuple]:
         return {
             cam: (self.config.cameras[cam].height, self.config.cameras[cam].width, 3) for cam in self.cameras
         }
-        
+
     @cached_property
     def observation_features(self) -> dict[str, type | tuple]:
         return {**self._motors_ft, **self._cameras_ft}
@@ -83,60 +85,64 @@ class DK1Follower(Robot):
     @cached_property
     def action_features(self) -> dict[str, type]:
         return self._motors_ft
-    
+
     @property
     def is_connected(self) -> bool:
         return self.bus_connected and all(cam.is_connected for cam in self.cameras.values())
-    
+
     def connect(self) -> None:
         if self.is_connected:
             raise DeviceAlreadyConnectedError(f"{self} already connected")
-        
-        self.serial_device = serial.Serial(self.config.port, 921600, timeout=0.5)
+
+        self.serial_device = serial.Serial(
+            self.config.port, 921600, timeout=0.5)
         time.sleep(0.5)
-        
+
         self.control = MotorControl(self.serial_device)
         self.bus_connected = True
         self.configure()
-            
+
         for cam in self.cameras.values():
             cam.connect()
-    
+
     @property
     def is_calibrated(self) -> bool:
         return True
-    
+
     def calibrate(self) -> None:
         pass
-    
+
     def configure(self) -> None:
-        
+
         for key, motor in self.motors.items():
             self.control.addMotor(motor)
-            
+
             for _ in range(3):
                 self.control.refresh_motor_status(motor)
                 time.sleep(0.01)
-            
+
             if self.control.read_motor_param(motor, DM_variable.CTRL_MODE) is not None:
                 print(f"{key} ({motor.MotorType.name}) is connected.")
-                
+
                 self.control.switchControlMode(motor, Control_Type.POS_VEL)
                 self.control.enable(motor)
             else:
-                raise Exception(f"Unable to read from {key} ({motor.MotorType.name}).")
-            
+                raise Exception(
+                    f"Unable to read from {key} ({motor.MotorType.name}).")
+
         for joint in ["joint_1", "joint_2", "joint_3"]:
-            # self.control.change_motor_param(self.motors[joint], DM_variable.ACC, 10.0)
-            # self.control.change_motor_param(self.motors[joint], DM_variable.DEC, -10.0)
-            self.control.change_motor_param(self.motors[joint], DM_variable.KP_APR, 160)
+            self.control.change_motor_param(self.motors[joint], DM_variable.ACC, 10.0)
+            self.control.change_motor_param(self.motors[joint], DM_variable.DEC, -10.0)
+            self.control.change_motor_param(self.motors[joint], DM_variable.KP_APR, 200)
             self.control.change_motor_param(self.motors[joint], DM_variable.KI_APR, 10)
-        
+
         for joint in ["gripper"]:
-            self.control.change_motor_param(self.motors[joint], DM_variable.KP_APR, 100)
-            
+            self.control.change_motor_param(
+                self.motors[joint], DM_variable.KP_APR, 100)
+
         # Open gripper and set zero position
-        self.control.switchControlMode(self.motors["gripper"], Control_Type.VEL)
+        self.control.switchControlMode(
+            self.motors["gripper"], Control_Type.VEL)
         self.control.control_Vel(self.motors["gripper"], 2.0)
         while True:
             self.control.refresh_motor_status(self.motors["gripper"])
@@ -149,25 +155,26 @@ class DK1Follower(Robot):
                 self.control.enable(self.motors["gripper"])
                 break
             time.sleep(0.01)
-        self.control.switchControlMode(self.motors["gripper"], Control_Type.POS_VEL)  
-        
-    
+        self.control.switchControlMode(
+            self.motors["gripper"], Control_Type.Torque_Pos)
+
     def get_observation(self) -> dict[str, Any]:
         if not self.is_connected:
             raise DeviceNotConnectedError(f"{self} is not connected.")
 
         # Read arm position
         start = time.perf_counter()
-        
+
         obs_dict = {}
         for key, motor in self.motors.items():
             self.control.refresh_motor_status(motor)
             if key == "gripper":
                 # Normalize gripper position between 1 (closed) and 0 (open)
-                obs_dict[f"{key}.pos"] = map_range(motor.getPosition(), self.gripper_open_pos, self.gripper_closed_pos, 0.0, 1.0)
+                obs_dict[f"{key}.pos"] = map_range(
+                    motor.getPosition(), self.gripper_open_pos, self.gripper_closed_pos, 0.0, 1.0)
             else:
-                obs_dict[f"{key}.pos"] = motor.getPosition()        
-        
+                obs_dict[f"{key}.pos"] = motor.getPosition()
+
         dt_ms = (time.perf_counter() - start) * 1e3
         logger.debug(f"{self} read state: {dt_ms:.1f}ms")
 
@@ -179,35 +186,30 @@ class DK1Follower(Robot):
             logger.debug(f"{self} read {cam_key}: {dt_ms:.1f}ms")
 
         return obs_dict
-    
-        
-    def send_action(self, action: dict[str, Any], V_desired: float = 0.5) -> dict[str, Any]:
+
+    def send_action(self, action: dict[str, Any]) -> dict[str, Any]:
         if not self.is_connected:
             raise DeviceNotConnectedError(f"{self} is not connected.")
 
-        goal_pos = {key.removesuffix(".pos"): val for key, val in action.items() if key.endswith(".pos")}
-
-        # Cap goal position when too far away from present position.
-        # /!\ Slower fps expected due to reading from the follower.
-        if self.config.max_relative_target is not None:
-            raise NotImplementedError("Max relative target is not implemented for the follower.")
-            present_pos = {}
-            for key, motor in self.motors.items():
-                self.control.refresh_motor_status(motor)
-                present_pos[key] = motor.getPosition()
-            
-            goal_present_pos = {key: (g_pos, present_pos[key]) for key, g_pos in goal_pos.items()}
-            goal_pos = ensure_safe_goal_position(goal_present_pos, self.config.max_relative_target)
+        goal_pos = {key.removesuffix(
+            ".pos"): val for key, val in action.items() if key.endswith(".pos")}
 
         # Send goal position to the arm
         for key, motor in self.motors.items():
             if key == "gripper":
+                self.control.refresh_motor_status(motor)
+                torque = motor.getTorque()
+                if abs(torque) > 1.0:
+                    logger.warning(f"Gripper torque is too high: {torque}")
                 goal_pos[key] = map_range(goal_pos[key], 0.0, 1.0, self.gripper_open_pos, self.gripper_closed_pos)
-            self.control.control_Pos_Vel(motor, goal_pos[key], V_desired)
-        
+                self.control.control_pos_force(motor, goal_pos[key], self.DM4310_SPEED*self.EMIT_VELOCITY_SCALE,
+                                               i_des=self.config.max_gripper_torque/self.DM4310_TORQUE_CONSTANT*self.EMIT_CURRENT_SCALE)
+            else:
+                self.control.control_Pos_Vel(
+                    motor, goal_pos[key], self.config.joint_velocity_scaling*self.DM4340_SPEED)
+
         return {f"{motor}.pos": val for motor, val in goal_pos.items()}
-    
-     
+
     def disconnect(self):
         if not self.is_connected:
             raise DeviceNotConnectedError(f"{self} is not connected.")
@@ -218,7 +220,7 @@ class DK1Follower(Robot):
         else:
             self.control.serial_.close()
         self.bus_connected = False
-            
+
         for cam in self.cameras.values():
             cam.disconnect()
 
